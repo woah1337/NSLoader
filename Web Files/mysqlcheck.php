8fc7903cb1318c5e9095d2acaf9dada22ba05065
woah1337 <40292926+woah1337@users.noreply.github.com>
2019-06-04 07:15:02 +0530

    Create mysqlcheck.php

---
+++ Web Files/mysqlcheck.php
@@ -0,0 +1,70 @@
+//use this if you don't want to use forum software
+//people can't register with this, maybe il add register sometime xd
+//pasted from mybbcheck.php cause idk php :/
+<?php
+$link = mysqli_connect("hostname", "root","password");
+$database = mysqli_select_db($link, "dbname");
+
+$user = $_GET['username'];
+$password = $_GET['password'];
+$hwid = mysqli_real_escape_string($link, $_GET['hwid']);
+$tables = "yourtablehere";
+
+$sql = "SELECT * FROM ". $tables ." WHERE username = '". mysqli_real_escape_string($link,$user) ."'" ;
+$result = $link->query($sql);
+if ($result->num_rows > 0) {
+    // Outputting the rows
+    while($row = $result->fetch_assoc())
+    {
+        
+        $password = $row['password'];
+        $plain_pass = $_GET['password'];
+        $stored_pass = $plain_pass;
+        
+        function Redirect($url, $permanent = false)
+        {
+            if (headers_sent() === false)
+            {
+                header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+            }
+        exit();
+        }
+        
+        if($stored_pass != $row['password'])
+        {
+            echo "p0<br>"; // Wrong pass, user exists
+        }
+        else
+        {
+            echo "p1<br>"; // Correct pass
+        }
+        
+        echo "g" . $row['usergroup'] . "<br>";
+
+        if (strlen($row['hwid']) > 1)
+        {
+            if ($hwid != $row['hwid'])
+            {
+                echo "h2"; // Wrong
+            }
+            else
+            {
+                echo "h1"; // Correct
+            }
+        }
+        else
+        {
+            $sql = "UPDATE ". $tables ." SET hwid='$hwid' WHERE username='$user'";
+            if(mysqli_query($link, $sql))
+            {
+                echo $row['hwid'];
+                echo "h3"; // HWID Set
+            }
+            else
+            {
+                echo "h4"; // Else errors
+            }
+        }
+    }
+}  
+?>
